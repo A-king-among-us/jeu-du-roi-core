f47413facb5e753f6f28841998ac7567394caf96
Maxime <maxleriche.60@gmail.com>
2020-08-10 12:57:12 +0200

    Mise en place de l'interface gage

---
+++ Library/Models/Gage/IGage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Library.Models.Gage
+{
+    public interface IGage
+    {
+        public string Status {get;set;}
+        public string LabelGage { get; set; }
+        public bool InvolveAll { get; set; }
+        public bool InvolveSpecialAction { get; set; }
+    }
+}
