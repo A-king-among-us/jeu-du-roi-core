4eb4a6b060c2b1cf21a927a08e6a468e6a8c978d
Maxime <maxleriche.60@gmail.com>
2020-08-27 00:42:41 +0200

    ajout de la fonction get turn

--- WebApp/Hubs/PartieHub.cs
+++ WebApp/Hubs/PartieHub.cs
@@ -59,13 +59,14 @@ namespace WebApp.Hubs
             }
             else
             {
-                string message = "Les stat transmise ne sont pas bonne"+(agi + str + vit + intel + perce);
+                string message = "Les stat transmise ne sont pas bonne"+(agi + str + vit + intel + perce-5);
                 if (NewPartie.PlayerDoesntExist(surname, Context.ConnectionId))
                     message = "Sois le joueur a déja un perso sois le nom de famille est déja pris";
                 await Clients.Caller.SendAsync("Error", "Erreur création du perso", message);
             }
         }
         public async Task GetMaxPointstat() => await Clients.Caller.SendAsync("MaxPointStat", Joueur.MaxPointStat);
+        public async Task GetTurn() => await Clients.Caller.SendAsync("Turn", NewPartie.Turn);
         public async Task GetMaxPerStat() => await Clients.Caller.SendAsync("MaxPerStat", Joueur.MaxPerStat);
         public async Task GetPlayeur(string surname) => await Clients.Caller.SendAsync("GetPlayeur", NewPartie.GetPlayeur(surname)); 
         public async Task GetMySelf() => await Clients.Caller.SendAsync("Iam", NewPartie.GetMyself(Context.ConnectionId));
